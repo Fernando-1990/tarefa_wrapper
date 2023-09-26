f0a7dbf6ef5414518ec69782587e456fe29c3f77
Fernando Ferreira <ferreira2007ster@gmail.co>
2023-09-26 16:04:33 -0300

    tarefa de Casting/boxing/wrapper

---
+++ src/Casting.java
@@ -0,0 +1,17 @@
+
+public class Casting {
+
+    private static void imprimeWrapper() {
+
+        short num = 10;
+        int n = num;
+
+
+        System.out.println("O Wrapper é: " + num);
+    }
+    public static void main(String[] args) {
+        imprimeWrapper();
+
+
+        }
+    }
